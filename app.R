64ceebff88ec7cd24ff581192f0848f835430a50
Paulina Garza <paugarza2208@gmail.com>
2025-12-10 18:57:23 -0600

    app

--- app.R
+++ app.R
@@ -4,27 +4,30 @@ library(readr)
 library(dplyr)
 
 ui <- fluidPage(
-    titlePanel("Auto MPG Explorer"),
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("xaxis", "Eje X", 
-                        choices = c("weight", "horsepower", "displacement")),
-            selectInput("color", "Color por", 
-                        choices = c("origin", "cylinders"))
-        ),
-        mainPanel(plotOutput("mainPlot"))
+  titlePanel("Auto MPG Explorer"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("xaxis", "Eje X",
+                  choices = c("weight", "horsepower", "displacement")),
+      selectInput("color", "Color por",
+                  choices = c("origin", "cylinders"))
+    ),
+    mainPanel(
+      plotOutput("mainPlot")
     )
+  )
 )
 
 server <- function(input, output) {
-    # Ahora leemos el CSV, no feather
-    data <- readr::read_csv("data/auto_mpg.csv")
 
-    output$mainPlot <- renderPlot({
-        ggplot(data, aes_string(x = input$xaxis, y = "mpg", color = input$color)) +
-            geom_point(size = 3) +
-            theme_minimal()
-    })
+  # Leemos el CSV (ya NO usamos arrow ni feather)
+  data <- readr::read_csv("data/auto_mpg.csv", show_col_types = FALSE)
+
+  output$mainPlot <- renderPlot({
+    ggplot(data, aes_string(x = input$xaxis, y = "mpg", color = input$color)) +
+      geom_point(size = 3) +
+      theme_minimal()
+  })
 }
 
-shinyApp(ui, server)
+shinyApp(ui = ui, server = server)
